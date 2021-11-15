cfea7ff1fdb862d506345bfe84be96618a36c4d5
Felipe Alvares de Azevedo Macedo <felipemacedo@corporigido.xyz>
2021-11-15 18:58:27 -0300

    UPD: Start of the exercise until lights.

--- 16-haunted-house/exercise/src/script.js
+++ 16-haunted-house/exercise/src/script.js
@@ -23,13 +23,82 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+//Group
+const house = new THREE.Group()
+scene.add(house)
+
+//Walls
+const walls = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(4,2.5,4),
+    new THREE.MeshStandardMaterial({color: '#ac8e82'})
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = 1.25
+house.add(walls)
+
+//Roof
+const roof = new THREE.Mesh(
+    new THREE.ConeBufferGeometry(3.5,1,4),
+    new THREE.MeshStandardMaterial({color: '#b35f45'})
+)
+roof.position.y = 3
+roof.rotation.y = Math.PI/4 
+house.add(roof)
+
+//Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2,2),
+    new THREE.MeshStandardMaterial({color: '#aa7b7b'})
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
+//Bushes
+const bushGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial({color: '#89c854'})
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5,0.5,0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25,0.25,0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4,0.4,0.4)
+bush3.position.set(-0.8, 0.1, 2.2)
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15,0.15,0.15)
+bush4.position.set(-1, 0.05, 2.6)
+
+house.add(bush1)
+house.add(bush2)
+house.add(bush3)
+house.add(bush4)
+
+//Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxBufferGeometry(0.6, 0.8, 0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({color: '#b2b6b1'})
+
+for(let i=0; i < 50; i++){
+    const angle = Math.random() * Math.PI*2
+    const radius = 3 + Math.random() * 6
+    const x = Math.sin(angle) *radius
+    const z = Math.cos(angle) *radius 
+
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.set(x, 0.3, z)
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+    graves.add(grave)
+}
+
 
 // Floor
 const floor = new THREE.Mesh(
