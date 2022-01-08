5eab8fd04b4229e5a0d8c6d1a3888f540ef04c0c
Felipe Macedo <felipeaamacedo@hotmail.com>
2022-01-07 21:51:53 -0300

    UPD: finish exercise

--- 17-particles/exercise/src/script.js
+++ 17-particles/exercise/src/script.js
@@ -19,15 +19,60 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+* Particles
+*/
+//Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 1000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i=0; i < count*3; i++){
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions,3)
 )
-scene.add(cube)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
+
+//Material
+const particleMaterial = new THREE.PointsMaterial()
+
+particleMaterial.size = 0.1
+particleMaterial.sizeAttenuation = true
+// particleMaterial.color = new THREE.Color('#ff88cc')
+// particleMaterial.map = particleTexture
+particleMaterial.transparent = true
+particleMaterial.alphaMap = particleTexture
+// particleMaterial.alphaTest = 0.001
+// particleMaterial.depthTest = false
+particleMaterial.depthWrite = false
+particleMaterial.blending = THREE.AdditiveBlending
+particleMaterial.vertexColors = true
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
+scene.add(particles)
+
+
+// Cube
+// const cube = new THREE.Mesh(
+    // new THREE.BoxBufferGeometry(),
+    // new THREE.MeshBasicMaterial()
+// )   
+// scene.add(cube)
 
 /**
  * Sizes
@@ -82,6 +127,19 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    //Update particles
+    // particles.rotation.y = - elapsedTime*0.02
+
+    //wave moviment
+    for(let i=0; i<count; i++){
+	const i3 = i*3
+
+	const x = particlesGeometry.attributes.position.array[i3]
+	particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+
+    particlesGeometry.attributes.position.needsUpdate = true
+    
     // Update controls
     controls.update()
 
