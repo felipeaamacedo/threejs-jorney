4ef2d94ab1bd6d25b36956aa71c6268d462af7b4
Felipe Alvares de Azevedo Macedo <felipemacedo@corporigido.xyz>
2022-01-16 17:23:25 -0300

    UPD: finished imported-models

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -2,6 +2,10 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+//import the DRACOLoader and copy the draco folder from '/node_modules/three/examples/js/libs/draco' to the project static folder
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader' 
 
 /**
  * Base
@@ -9,11 +13,82 @@ import * as dat from 'lil-gui'
 // Debug
 const gui = new dat.GUI()
 
-// Canvas
+// Canvas 
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+    // '/models/Duck/glTF/Duck.gltf', //DUCK Example
+    // '/models/FlightHelmet/glTF/FlightHelmet.gltf', //HELMET Example
+    // '/models/Duck/glTF-Draco/Duck.gltf', //DRACO Example
+    '/models/Fox/glTF/Fox.gltf',
+    (gltf) => {
+        console.log('success')
+
+        /* 
+        // UNCOMENT BLOCK to load first Duck.gltf example
+        scene.add(gltf.scene.children[0])
+        */
+       
+        //IMPORTANTE! To add every child mesh you should while until there is no mesh inside the children array. Every time you use "scene.add()" you take one mesh from the array and reduce its lenght. That's why the for(child of gltf.scene.children) does not work.
+
+        //2nd Option: use a while loop and add until the gltf.scene.child length is less then zero.
+        /*
+        while(gltf.scene.children.length){
+            scene.add(gltf.scene.children[0])
+        }
+        */
+
+        // 3rd OPTION: use the spread operator and copy gltf.scene.children to a constant and use the for loop into it.
+        /*
+        //UNCOMENT BLOCK TO LOAD THE HELMET
+        const children = [...gltf.scene.children]
+        for(const child of children){
+            scene.add(child)
+        }
+        */
+
+        // 4th OPTION: load the whole scene, it is faster coding, though you will need to load a full scene. USED IN THE DRACO EXAMPLE
+        // scene.add(gltf.scene)
+
+        //FOX Example
+        mixer = new THREE.AnimationMixer(gltf.scene)
+        const action = mixer.clipAction(gltf.animations[0])
+
+        //Uncomment to change in between the animations
+        // const action = mixer.clipAction(gltf.animations[1])
+        // const action = mixer.clipAction(gltf.animations[2])
+
+        action.play()
+
+        gltf.scene.scale.set(0.025, 0.025, 0.025)
+        scene.add(gltf.scene)
+
+
+
+    },
+    (progress) =>{
+        console.log('progress')
+        console.log(progress)
+    },
+    (error) => {
+        console.log('error')
+        console.log(error)
+    }
+)
 
 /**
  * Floor
@@ -105,6 +180,11 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    // Update mixer
+    if(mixer !== null){
+        mixer.update(deltaTime)
+    }
 
     // Update controls
     controls.update()
